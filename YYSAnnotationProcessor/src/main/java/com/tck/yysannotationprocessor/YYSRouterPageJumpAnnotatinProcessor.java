1df4af7d6c77db52cf8dac87f89d8229838683dc
tck <tck@dra100.com>
2018-11-30 13:44:36 +0800

    修改1

--- YYSAnnotationProcessor/src/main/java/com/tck/yysannotationprocessor/YYSRouterPageJumpAnnotatinProcessor.java
+++ YYSAnnotationProcessor/src/main/java/com/tck/yysannotationprocessor/YYSRouterPageJumpAnnotatinProcessor.java
@@ -1,16 +1,20 @@
 package com.tck.yysannotationprocessor;
 
 import com.google.auto.service.AutoService;
+import com.tck.yysannotation.YYSRouterPage;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 
 
 /**
@@ -21,6 +25,8 @@ import javax.lang.model.element.TypeElement;
 @AutoService(javax.annotation.processing.Processor.class)
 public class YYSRouterPageJumpAnnotatinProcessor extends AbstractProcessor {
 
+    private Map<Element, List<VariableElement>> items = new HashMap<>();
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
@@ -30,6 +36,19 @@ public class YYSRouterPageJumpAnnotatinProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
 
+        Set<? extends Element> elementsAnnotatedWith = roundEnvironment.getElementsAnnotatedWith(YYSRouterPage.class);
+        for (Element element : elementsAnnotatedWith) {
+            //主要获取ElementType 是不是null，即class，interface，enum或者注解类型
+            if (element.getEnclosingElement() == null) {
+                //直接结束处理器
+                return true;
+            }
+
+            //如果items的key不存在，则添加一个key
+            if (!items.containsKey(element.getEnclosingElement())){
+                items.put(element.getEnclosingElement(), new LinkedList<VariableElement>());
+            }
+        }
         return false;
     }
 }
