af8ffb3b28528ccab2e945b034e4af7f94859d64
tck8888 <380054728@qq.com>
2019-05-20 23:59:51 +0800

    添加YRoute类

---
+++ router/src/main/java/com/tck/router/IRouter.java
@@ -0,0 +1,13 @@
+package com.tck.router;
+
+/**
+ * <p>description:</p>
+ * <p>created on: 2019/5/20</p>
+ *
+ * @author tck
+ * @version 1.0
+ */
+public interface IRouter {
+
+    void putActivity();
+}
--- router/src/main/java/com/tck/router/YRouter.java
+++ router/src/main/java/com/tck/router/YRouter.java
@@ -1,5 +1,10 @@
 package com.tck.router;
 
+import android.app.Activity;
+import android.text.TextUtils;
+
+import java.util.HashMap;
+
 /**
  * <p>description:</p>
  * <p>created on: 2019/5/20</p>
@@ -8,5 +13,25 @@ package com.tck.router;
  * @version 1.0
  */
 public class YRouter {
-    
+
+    private HashMap<String, Class<? extends Activity>> activityList;
+
+    public static YRouter yRouter = new YRouter();
+
+    private YRouter() {
+        activityList = new HashMap<>();
+    }
+
+    public static YRouter getInstance() {
+        return yRouter;
+    }
+
+    public void putActivity(String path, Class<? extends Activity> clazz) {
+        if (!TextUtils.isEmpty(path)) {
+            return;
+        }
+
+        activityList.put(path, clazz);
+    }
+
 }
